828d77e1e4ce0277cba2f38fc022ed705b59f7f3
John Fairclough <johnmfairclough@gmail.com>
2020-04-02 14:21:46 +0100

    initial

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+    <h1>About This App</h1>
+    <p>An Application to search for Github user profiles</p>
+    <p>Version: 1.0.1</p>
+    <p>Built by: John Fairclough</p>
+    <a href="https:www.jfairclough.com">jfairclough.com</a>
+    </Fragment>
+  );
+};
+
+export default About;
